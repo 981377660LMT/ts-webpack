c07631d616115f902833a0bee7b92f12479c2b68
草莓奶昔 <8306432+caomeinaixi@user.noreply.gitee.com>
2021-06-02 23:05:26 +0800

    修改配置

---
+++ config/webpack/webpack.common.js
@@ -0,0 +1,51 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ProgressPlugin = require('progress-webpack-plugin')
+
+const { entries, htmlTemplates, srcPath } = require('./getPath')
+
+module.exports = {
+  entry: entries,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            id: 'js',
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        include: srcPath,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true,
+            happyPackMode: true,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ProgressPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+    ...htmlTemplates,
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+    mainFiles: ['index', 'main'],
+    alias: {
+      '@': srcPath,
+    },
+  },
+}
